e3837416edddd2020faf023fb709492394229b46
Carlos Campoy <campoy.miro@gmail.com>
2022-06-21 15:30:42 -0400

    v0.1 working

--- public/main.js
+++ public/main.js
@@ -1,1 +1,28 @@
-console.log('testing')
+let price = document.getElementById('price')
+let quantity = document.getElementById('quantity')
+let output = document.getElementById('output')
+
+const showCalc = _ => {
+  const getUnit = _ => {
+    return document.getElementById('radio-lb').checked
+    ? 'lb'
+    : 'oz'
+  }
+
+  return quantity.value
+  ? output.textContent = `$${pricePerPound(price.value, quantity.value, getUnit())} per lb`
+  : output.textContent = `Enter quantity`
+}
+
+const ozToPounds = oz => {
+  const lb = oz * 0.0625
+  return lb
+}
+
+const pricePerPound = (price, quantity, unit) => {
+  return unit === 'oz'
+  ? price / ozToPounds(quantity)
+  : price / quantity
+}
+
+quantity.oninput = showCalc
